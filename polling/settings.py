d09824ec2217deaa4205830babf875e03f669ad8
shuja_akbar <shujaakbar1415@gmail.com>
2019-07-20 11:28:38 +0000

    CHanging host name

--- polling/settings.py
+++ polling/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'u32#uj67u(9*mk$#%1)&!)s7(^frg39#2(!=x_%1((3ig3)f(k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['first-polls-app.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['first-polls-app.herokuapp.com']
 
 
 # Application definition
@@ -43,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,7 +129,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
